b9c4f5c3940ec15e03eb1ac7067f16b59f85ac91
phonpadid <phonpadid669988@gmail.com>
2024-02-09 15:24:23 +0700

    Update

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <RouterLink to="/">Home</RouterLink>
+  <RouterLink to="/profile">Profile</RouterLink>
+  <RouterView />
+</template>
